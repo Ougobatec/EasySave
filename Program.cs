b9dd8aa7f99e0e6bd20af85da51c47f93127f244
calaxo <36140542+calaxo@users.noreply.github.com>
2025-02-03 16:37:07 +0100

    logger

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using EasySave;
+
+
+
+namespace EasySave
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+
+            Console.WriteLine("Hello, World!");
+
+
+
+
+            Singleton monsigleton = new Singleton();
+
+
+            monsigleton.MonLogger.Info("eee");
+
+            monsigleton.MonLogger.Info("ssssssssssseee");
+
+            monsigleton.MonLogger.Info("ssssssssssseee");
+
+            monsigleton.MonLogger.Error("ssssscazefdzefzefzefezfzefsssssseee");
+
+            monsigleton.MonLogger.Warning("ssssssssssseee");
+
+            monsigleton.MonLogger.Trace("ssssssssssseee");
+
+            monsigleton.MonLogger.Debug("ssssssssssseee");
+
+
+
+        }
+
+
+
+
+    }
+
+}
---
+++ logger/ThonToutSeul.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace EasySave
+{
+
+
+
+    class Singleton
+    {
+        private string souschemin = "premiertruc.Json";
+        private string docPath = AppDomain.CurrentDomain.BaseDirectory;
+        string chemin;
+        public Logger MonLogger;
+
+        public Singleton()
+        {
+            chemin = Path.Combine(docPath, souschemin);
+            Console.WriteLine(chemin);
+            MonLogger = new Logger(chemin);
+        }
+    }
+}
