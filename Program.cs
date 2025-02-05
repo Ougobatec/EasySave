3bb27e4766f76aaed6ebd0b3c7538f4c104e28d5
calaxo <36140542+calaxo@users.noreply.github.com>
2025-02-05 11:11:07 +0100

    marche

--- Program.cs
+++ Program.cs
@@ -1,1 +1,40 @@
 
+using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        BackupManager manager = new BackupManager();
+
+        // Ajout de quelques tâches de sauvegarde
+        BackupJob job1 = new BackupJob(
+                    "Sauvegarde Documents",
+                    "C:\\Users\\axelc\\Desktop\\dev\\EasySave\\DataManipulation\\Source1",
+                    "C:\\Users\\axelc\\Desktop\\dev\\EasySave\\DataManipulation\\Backup1",
+                    BackupType.Complete
+                );
+
+        BackupJob job2 = new BackupJob(
+            "Sauvegarde Images",
+            "C:\\Users\\axelc\\Desktop\\dev\\EasySave\\DataManipulation\\Source2",
+            "C:\\Users\\axelc\\Desktop\\dev\\EasySave\\DataManipulation\\Backup2",
+            BackupType.Differential
+        );
+
+
+
+        manager.AddBackupJob(job1);
+        manager.AddBackupJob(job2);
+
+        // Exécution de toutes les sauvegardes
+        manager.RunAllBackups();
+
+        // Affichage du statut des sauvegardes
+        manager.DisplayStatus();
+    }
+}
+
+
+//C: \Users\axelc\Desktop\dev\EasySave\DataManipulation
