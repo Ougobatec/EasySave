70dcda62bdfeca0c4c6e25a5fed0416a1f014e44
calaxo <36140542+calaxo@users.noreply.github.com>
2025-02-12 10:58:01 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,3 +11,19 @@ namespace EasySave
         }
     }
 }
+
+public static string GetFolderPath(Environment.SpecialFolder folder);
+
+Console.WriteLine(HashFromFile(new FileInfo(newPath)));
+
+
+public static string GetTempPath();
+string result = Path.GetTempPath();
+Console.WriteLine(result);
+
+
+static string HashFromFile(FileInfo file)
+{
+    byte[] firstHash = MD5.Create().ComputeHash(file.OpenRead());
+    return Encoding.Default.GetString(firstHash);
+}
